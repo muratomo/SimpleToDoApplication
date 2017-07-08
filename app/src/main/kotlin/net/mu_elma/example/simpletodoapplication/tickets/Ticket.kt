9e74a43ac2903a765ea24b54f7ade1c7e99dd6bc
muratomo <muratomo40424@gmail.com>
2017-07-08 14:01:57 +0900

    make tickets

---
+++ app/src/main/kotlin/net/mu_elma/example/simpletodoapplication/tickets/Ticket.kt
@@ -0,0 +1,16 @@
+package net.mu_elma.example.simpletodoapplication.tickets
+
+import java.util.*
+
+/**
+ * Created by muratomo on 2017/07/08.
+ */
+
+// ToDo詳細 Ticketという名前で管理
+data class Ticket (
+        val title: String,
+        val status: TicketStatus,
+        val priority: TicketPriority,
+        val limit: Date,
+        val content: String
+)
